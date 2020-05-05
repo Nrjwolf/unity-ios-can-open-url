010ce62fcc0a212a11b5ddf1579615a239c0eed5
Alexander <nrjwolf@gmail.com>
2020-05-05 20:35:02 +0800

    init

---
+++ Plugin/IOSCanOpenURL/IOS/IOSCanOpenURL.cs
@@ -0,0 +1,13 @@
+﻿#if UNITY_IOS 
+using System.Runtime.InteropServices;
+#endif
+
+public static class IOSCanOpenURL
+{
+#if UNITY_IOS && !UNITY_EDITOR
+    [DllImport("__Internal")] private static extern bool _IOSCanOpenURL(string url);
+    public static bool CheckUrl (string url) => _IOSCanOpenURL (url);
+#else
+    public static bool CheckUrl(string url) => false; 
+#endif
+}
---
+++ Plugin/IOSCanOpenURL/IOS/IOSCanOpenURL.m
@@ -0,0 +1,8 @@
+﻿NSString *ToNSString(char* string) {
+    return [NSString stringWithUTF8String:string];
+}
+
+/// https://developer.apple.com/documentation/uikit/uiapplication/1622952-canopenurl?language=objc
+bool _IOSCanOpenURL (char* url) {
+    return [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:ToNSString(url)]];
+}
